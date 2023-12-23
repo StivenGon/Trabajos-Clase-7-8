6658a5636f48c898751267e19a23a86469f53184
StivenGon <oscarstiven.2005@gmail.com>
2023-12-22 23:13:50 -0300

    Ejercicios

---
+++ practica07/src/practica07/Practica07.java
@@ -0,0 +1,16 @@
+
+package practica07;
+
+/**
+ * @author Stiven Gonzalez
+ */
+public class Practica07 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
